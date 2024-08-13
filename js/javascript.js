00a6bb264067480cddc2ff07211450ae9aed94f8
AgustinPonce1 <136716814+AgustinPonce1@users.noreply.github.com>
2024-08-12 22:30:14 -0300

    VERSION PRE ENTREGA Nº3

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,27 @@
-/* JAVASCRIPT - SOF SHOES */
+/* JAVASCRIPT - SOF SHOES */       
+
+/* CARRUSEL INICIO */s
+$(document).ready(function(){
+    $('.product-carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
